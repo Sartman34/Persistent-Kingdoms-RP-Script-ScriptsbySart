1e932f40cba20d1318480c10acb31204a04abfc3
Sartman34 <andach.elmaskaya@gmail.com>
2024-08-06 14:42:17 +0300

    update

--- TCP Server Source/açlık scripti.py
+++ TCP Server Source/açlık scripti.py
@@ -2,12 +2,16 @@ import socket
 import time
 
 ip_adress = "127.0.0.1"
+with open("Data\\hunger.txt", "r") as file:
+    data = file.read().split("\n")
+is_enabled = int(data.pop(0).split(" : ")[1])
+interval = int(data.pop(0).split(" : ")[1])
 
-while True:
-    time.sleep(180)
-    #input()
+while is_enabled:
+    time.sleep(interval * 60)
     server = socket.socket()
     server.connect((ip_adress, 80))
     server.send("A /sts%3C13".encode())
     print("Got:  " + server.recv(1024).decode())
     server.close()
+input("Hunger is not enabled.")
