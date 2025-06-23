78f92782443ea0f15ccb5b8d7441e7d207e3739b
Sart <67769292+Sartman34@users.noreply.github.com>
2025-06-24 00:17:13 +0300

    linux setup

--- _pk script start_linux.sh
+++ _pk script start_linux.sh
@@ -1,1 +1,2 @@
-wine mb_warband_wse2_dedicated.exe --config-path server_config.ini -r _pk_script.txt --module Persistent Kingdoms
+#!/bin/bash
+WINEDEBUG=fixme-all nohup wine mb_warband_wse2_dedicated.exe --config-path server_config.ini -r _pk_script.txt --module Persistent Kingdoms &
