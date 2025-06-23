084ff53834d586f960580bf839c0dcc47914b752
Server <andach2002@hotmail.com>
2025-06-23 21:35:10 +0000

    start linux sh

--- _pk script start_linux.sh
+++ _pk script start_linux.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-WINEDEBUG=fixme-all nohup wine mb_warband_wse2_dedicated.exe --config-path server_config.ini -r _pk_script.txt --module Persistent Kingdoms &
+gnome-terminal -- bash -c "
+WINEDEBUG=fixme-all nohup wine mb_warband_wse2_dedicated.exe --config-path server_config.ini -r _pk_script.txt --module Persistent Kingdoms > /dev/null 2>&1 &
+exec bash"
