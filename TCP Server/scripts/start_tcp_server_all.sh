dbacc98543220f20a06e393585f2a6722d5d476f
Server <andach2002@hotmail.com>
2025-06-25 11:36:05 +0000

    linux start

--- TCP Server/scripts/start_tcp_server_all.sh
+++ TCP Server/scripts/start_tcp_server_all.sh
@@ -4,3 +4,4 @@ source source/env/bin/activate
 gnome-terminal -- bash -c "python ./source/ana\ script.py"
 gnome-terminal -- bash -c "python ./source/autosave.py"
 gnome-terminal -- bash -c "python ./source/açlık\ scripti.py"
+gnome-terminal -- bash -c "python ./source/Server\ Owner\ Panel\ v1.3.3\ x64.py"
